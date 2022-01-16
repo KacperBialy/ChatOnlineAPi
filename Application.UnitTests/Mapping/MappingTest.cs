872d248ee1cafafabc285fdca3dc1f6cfd9e27f5
Kacper Biały <44371092+KacperBialy@users.noreply.github.com>
2022-01-16 20:47:25 +0100

    Adding mapper configuration test

---
+++ Application.UnitTests/Mapping/MappingTest.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Application.UnitTests.Mapping
+{
+    public class MappingTest : IClassFixture<MappingTestFixture>
+    {
+        private readonly IConfigurationProvider _configurationProvider;
+        private readonly IMapper _mapper;
+
+        public MappingTest(MappingTestFixture fixture)
+        {
+            _configurationProvider = fixture.ConfigurationProvider;
+            _mapper = fixture.Mapper;
+        }
+
+        [Fact]
+        public void ShouldHaveValidConfiguration()
+        {
+            _configurationProvider.AssertConfigurationIsValid();
+        }
+    }
+}
---
+++ Application.UnitTests/Mapping/MappingTestFixture.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using ChatOnline.Application.Common.Mappings;
+
+namespace Application.UnitTests.Mapping
+{
+    public class MappingTestFixture
+    {
+        public MappingTestFixture()
+        {
+            ConfigurationProvider = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+
+            Mapper = ConfigurationProvider.CreateMapper();
+        }
+        public IConfigurationProvider ConfigurationProvider { get; set; }
+        public IMapper Mapper { get; set; }
+    }
+}
